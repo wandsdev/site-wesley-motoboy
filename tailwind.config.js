4118906ce64d36164332ceccf2ddba4d85d2acc0
Wanderlei <wanderlei.junior@teknisa.com>
2022-10-14 21:21:17 -0300

    envio de email implementado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './sucesso.html'],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
